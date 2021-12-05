fb3bfe620b6251b71865b211b9dadbc40534b330
Roman Broshnitskiy <romario777v8@gmail.com>
2021-12-05 18:24:31 +0200

    переміщує запити в один файл

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../sass/main.scss';
 import { instance } from "./team-members";
-import { getPopularFilms } from "./services/fetch-popular-films";
-import { getGenres } from "./services/fetch-genres";
+import { getPopularFilms } from "./services/fetch-backend";
+import { getGenres } from "./services/fetch-backend";
 
 const refs = {
   developer: document.querySelector(".footer-link"),
@@ -21,7 +21,7 @@ getGenres()
   })
   .catch(error => console.log(error))
 
-getPopularFilms(20)
+getPopularFilms(1)
   .then(films => {
     console.log(films.results)
     localStorage.setItem("films_from_beckend", JSON.stringify(films.results));
@@ -34,22 +34,23 @@ getPopularFilms(20)
 function markUpFilmCards(films) {
   const genres = localStorage.getItem("genres");
   const parsedGenres = JSON.parse(genres);
+  console.log(parsedGenres)
  
   const markUp = films.map(film => {
-    const { title, genre_ids, vote_average, release_date, poster_path, backdrop_path } = film;
+    const { title, name, genre_ids, vote_average, release_date, first_air_date, poster_path, backdrop_path } = film;
     // console.log(parsedGenres)
     // console.log(genre_ids)
-    // const genresById = parsedGenres.filter(({id})=> genre_ids.includes(id));
-
+    // let genresById = parsedGenres.filter(({ id }) =>  id>25 );
+    // console.log(genresById)
     // if (genresById.length > 3) {
     //   genresById.splice(2, 2, "other")
     // }
     // console.log(genresById)
     return `<div class="">
     <div class=""><img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="poster-film"></div>
-      <h2 class="">${title.toUpperCase()}</h2>
+      <h2 class="">${title.toUpperCase() || name.toUpperCase()}</h2>
       <div class="">
-        <p class="">${2} | ${release_date.slice(0, 4)}</p>
+        <p class="">${5} | ${release_date.slice(0, 4) || first_air_date.slice(0, 4)}</p>
         <p class="">${vote_average}</p>
       </div>
       </div>`;
--- src/js/services/fetch-backend.js
+++ src/js/services/fetch-backend.js
@@ -5,3 +5,9 @@ export async function getPopularFilms(page) {
     const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${page}`);
     return response.data;
 };
+
+
+export async function getGenres() {
+    const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+    return response.data;
+};
