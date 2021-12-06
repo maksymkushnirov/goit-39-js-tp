5902ada7c121824e2c620f76e389aa9e5ab01f14
Taras-Roboteckyi <rtvr@ukr.net>
2021-12-06 15:51:56 +0200

    Додає надпис до сторінки My library

--- src/js/onclick-my_library.js
+++ src/js/onclick-my_library.js
@@ -6,6 +6,7 @@ const refs = {
     openNextBtn: document.querySelector('.header-change__cont'),//Пошук контейнера з кнопками Watchd і Queve в Header 
     btnWatched: document.querySelector('[data-watched]'),//Пошук кнопки Watched в Header 
     btnQueue: document.querySelector('[data-queue]'),//Пошук кнопки Queve в Header 
+    gallery: document.querySelector(".gallery")
     };
 
     refs.btnLibrary.addEventListener('click', onBtnLibrary)//Виклик слухача на кнопку LIBRARY в Header 
@@ -14,12 +15,13 @@ const refs = {
    
      //Функція виконується при нажиманні на кнопку My Library
 export function onBtnLibrary() {
-    /* refs.btnHome.classList.remove('nav-item__list--activ');
+    refs.btnHome.classList.remove('nav-item__list--activ');
     refs.btnLibrary.classList.add('nav-item__list--activ');
     refs.searchForm.classList.add('visually-hidden');
     refs.openNextBtn.classList.remove('visually-hidden');
     refs.changeHeader.classList.add('header-change');
-    onBtnInMyLibrary(); */
+    onBtnInMyLibrary();
+    markUpLibraryScreen();
 }
 
 //Функція - переключатель на кнопки Watchd і Queve в My LIBRARY 
@@ -53,4 +55,15 @@ function onBtnQueueInMyLibrary() {
     refs.btnWatched.classList.remove('header-change__cont-btn--activ');
     refs.btnQueue.classList.add('header-change__cont-btn--activ');
     
+}
+//Функція - рендер пустого екрана My Library
+function markUpLibraryScreen() {
+    if (localStorage.getItem("key") === null || localStorage.getItem("key") === null ) {//Потрібно поєднати ключ localStorage з Дмитром
+        refs.gallery.innerHTML =
+        `<p class="library-screen__text">The library is currently empty!</p>
+        <img src="./images/svg/film.svg" alt="logo" />`
+    } else {
+        //додається функція Дмитра//
+    }
+  
 }
