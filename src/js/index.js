ce5c9d4d9ca466dd39b6474c3cb8bb6bdc6d873e
Roman Broshnitskiy <romario777v8@gmail.com>
2021-12-06 19:41:07 +0200

    заміняє жанри по id

--- src/js/index.js
+++ src/js/index.js
@@ -3,62 +3,47 @@ import { instance } from "./team-members";
 import { getPopularFilms } from "./services/fetch-backend";
 import { getGenres } from "./services/fetch-backend";
 import { onBtnLibrary } from './onclick-my_library';
+import { markUpPopularFilmGallery } from "./mark_up_popular_film_gallery";
 
 let page = 1;
 const refs = {
   developer: document.querySelector(".footer-link"),
-  gallery: document.querySelector(".card-list")
 };
 
+//Показує список розробників сайту при кліку на ссилку в футері
 refs.developer.addEventListener("click", (e) => {
   e.preventDefault();
   instance.show();
-
 })
 
 onBtnLibrary()//Функція виконується при нажиманні на кнопку My Library
 
-getGenres()
+getGenres() //Функція приймає жанри з бекенду і записує в локальне сховище
   .then(value => {
     localStorage.setItem("genres", JSON.stringify(value));
   })
   .catch(error => console.log(error))
 
-getPopularFilms(page)
+getPopularFilms(page) //Функція приймає популярні фільми з бекенду(перші 20), рендерить їх в html і записує в локальне сховище
   .then(films => {
-    console.log(films.results)
+    films.results.map(film => {
+      const genres = localStorage.getItem("genres");
+      const parsedGenres = JSON.parse(genres);
+      const genresById = parsedGenres.genres.filter(({ id }) => film.genre_ids.includes(id));
+      const arrayGenres = genresById.map(genre => {
+        return  genre.name
+      })
+        if (arrayGenres.length > 3) {
+          arrayGenres.splice(2, 10, "other...")
+        }
+      film.genre_ids = arrayGenres;
+    })
+  
     localStorage.setItem("films_from_beckend", JSON.stringify(films.results));
-    markUpFilmCards(films.results);
+    markUpPopularFilmGallery(films.results);
   })
   .catch(error => console.log(error))
 
  
 
-function markUpFilmCards(films) {
-  const genres = localStorage.getItem("genres");
-  const parsedGenres = JSON.parse(genres);
-  console.log(parsedGenres)
- 
-  const markUp = films.map(film => {
-    const { title, name, genre_ids, vote_average, release_date, first_air_date, poster_path, backdrop_path } = film;
-    // console.log(parsedGenres)
-    // console.log(genre_ids)
-    // let genresById = parsedGenres.filter(({ id }) =>  id>25 );
-    // console.log(genresById)
-    if (genre_ids.length > 3) {
-      genre_ids.splice(2, 10, "other...")
-    }
-    // console.log(genresById)
-    return `<li class="card-item">
-        <a class="link" href="#">
-        <div class=""><img class="card-image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="poster-film"></div>
-        <h2 class="card-title">${title.toUpperCase() || name.toUpperCase()}</h2>
-          <div class="card-description-container">
-            <p class="card-description">${genre_ids.join(", ")} | ${release_date.slice(0, 4) || first_air_date.slice(0, 4)}</p>
-          </div>
-      </a>
-    </li>`;
-  }).join("");
 
-  refs.gallery.insertAdjacentHTML("beforeend", markUp)
-};
