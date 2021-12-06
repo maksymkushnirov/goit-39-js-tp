c4b9c0c83d7a022c3aaaf1f5005b98e2bf8742e6
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-12-06 23:15:18 +0200

    update prettier configuration

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,38 @@
-import "../sass/main.scss";
-import { instance } from "./team-members";
-import { getPopularFilms } from "./services/fetch-backend";
-import { getGenres } from "./services/fetch-backend";
+import '../sass/main.scss';
+import { instance } from './team-members';
+import { getPopularFilms } from './services/fetch-backend';
+import { getGenres } from './services/fetch-backend';
 
-import { replaceGenresById } from "./services/replace_genres_by_id";
+import { replaceGenresById } from './services/replace_genres_by_id';
 import { onBtnLibrary } from './onclick-my_library';
-import { markUpPopularFilmGallery } from "./mark_up_popular_film_gallery";
+import { markUpPopularFilmGallery } from './mark_up_popular_film_gallery';
 
 let page = 1;
 const refs = {
-  developer: document.querySelector(".footer-link"),
+  developer: document.querySelector('.footer-link')
 };
 
 //Показує список розробників сайту при кліку на ссилку в футері
-refs.developer.addEventListener("click", (e) => {
+refs.developer.addEventListener('click', (e) => {
   e.preventDefault();
   instance.show();
-})
+});
 
 // onBtnLibrary()//Функція виконується при нажиманні на кнопку My Library
 
 getGenres() //Функція приймає жанри з бекенду і записує в локальне сховище
-  .then(value => {
-    localStorage.setItem("genres", JSON.stringify(value));
+  .then((value) => {
+    localStorage.setItem('genres', JSON.stringify(value));
   })
-  .catch(error => console.log(error))
+  .catch((error) => console.log(error));
 
 getPopularFilms(page) //Функція приймає популярні фільми з бекенду(перші 20), рендерить їх в html і записує в локальне сховище
-  .then(films => {
-    films.results.map(filmData => {
+  .then((films) => {
+    films.results.map((filmData) => {
       replaceGenresById(filmData); //Функція добавляє назви жанрів по id
-    })
-  
-    localStorage.setItem("films_from_beckend", JSON.stringify(films.results));
+    });
+
+    localStorage.setItem('films_from_beckend', JSON.stringify(films.results));
     markUpPopularFilmGallery(films.results);
   })
-  .catch(error => console.log(error))
-
- 
-
-
-
-
+  .catch((error) => console.log(error));
