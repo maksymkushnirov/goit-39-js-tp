bfab194afdf697678a43e99d13c11a7fc6a96515
Taras-Roboteckyi <rtvr@ukr.net>
2021-12-06 23:29:45 +0200

    Додає ведмедя на сторінку))

--- src/js/onclick-my_library.js
+++ src/js/onclick-my_library.js
@@ -1,3 +1,5 @@
+import  img  from "../images/screen-library.jpg";
+
 const refs = {
     btnHome: document.querySelector('[data-home]'), //Пошук кнопки HOME в Header
     btnLibrary: document.querySelector('[data-library]'),//Пошук кнопки LIBRARY в Header
@@ -6,11 +8,11 @@ const refs = {
     openNextBtn: document.querySelector('.header-change__cont'),//Пошук контейнера з кнопками Watchd і Queve в Header 
     btnWatched: document.querySelector('[data-watched]'),//Пошук кнопки Watched в Header 
     btnQueue: document.querySelector('[data-queue]'),//Пошук кнопки Queve в Header 
-    gallery: document.querySelector(".gallery")
+    gallery: document.querySelector(".gallery")//Пошук контейнера gallery в main 
     };
 
     refs.btnLibrary.addEventListener('click', onBtnLibrary)//Виклик слухача на кнопку LIBRARY в Header 
-   
+   // refs.btnLibrary.removeEventListener('click', onBtnLibrary)
    
    
      //Функція виконується при нажиманні на кнопку My Library
@@ -22,6 +24,7 @@ export function onBtnLibrary() {
     refs.changeHeader.classList.add('header-change');
     onBtnInMyLibrary();
     markUpLibraryScreen();
+    
 }
 
 //Функція - переключатель на кнопки Watchd і Queve в My LIBRARY 
@@ -35,14 +38,14 @@ function onBtnInMyLibrary() {
     }
    else {
         onBtnQueueInMyLibrary();
-        return refs.btnQueue.removeEventListener('click', onBtnInMyLibrary);
+                 return refs.btnQueue.removeEventListener('click', onBtnInMyLibrary);
        
     };
     
-     
-    
 }
- 
+
+
+
 //Функція виконується при нажиманні на кнопку Watched
 function onBtnWatchedInMyLibrary() {
     refs.btnWatched.classList.add('header-change__cont-btn--activ');
@@ -59,11 +62,17 @@ function onBtnQueueInMyLibrary() {
 //Функція - рендер пустого екрана My Library
 function markUpLibraryScreen() {
     if (localStorage.getItem("key") === null || localStorage.getItem("key") === null ) {//Потрібно поєднати ключ localStorage з Дмитром
+       /*  refs.gallery.innerHTML =
+        `<p class="library-screen__text">The library is currently empty!</p>
+        <img class="library-screen__image" src="https://assets.materialup.com/uploads/b17ea0c7-df76-4ce1-bf82-4a2cf6ae866d/preview.jpg" alt="Bear" />` */
         refs.gallery.innerHTML =
         `<p class="library-screen__text">The library is currently empty!</p>
-        <img src="https://assets.materialup.com/uploads/b17ea0c7-df76-4ce1-bf82-4a2cf6ae866d/preview.jpg" alt="logo" />`
+        <img class="library-screen__image" src="${img}" alt="Bear" />`
     } else {
         //додається функція Дмитра//
     }
   
 }
+
+
+
