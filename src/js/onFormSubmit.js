e6b52eed201e7a952fa9def16d9537f1c3a01521
Taras-Roboteckyi <rtvr@ukr.net>
2021-12-09 02:35:11 +0200

    Вдалось зарендерити фільми по запиту в формі!

--- src/js/onFormSubmit.js
+++ src/js/onFormSubmit.js
@@ -1,35 +1,72 @@
 // import { refs } from './refs';
-// import { getSearch } from './services/fetch-backend';
-import axios from 'axios';
+import { getSearch } from './services/fetch-backend';
+import { replaceGenresById } from './services/replace_genres_by_id';
+import { markUpPopularFilmGallery } from './mark_up_popular_film_gallery';
 
- async function getSearch(value) {
-  const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${value}`);
-   return response.data
-   
-}
+
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
+  gallery: document.querySelector('.card-list'),
   error: document.querySelector('.header-error__text'),
 };
 
 refs.searchForm.addEventListener('submit', onSubmit);
 
 
+
+
+//Функція виконується при вводі в інпуті
 async function onSubmit(event) {
   event.preventDefault();
-  const inputValue = event.srcElement[0].value;
-
+  
+  clearMarkup();
+  
+  if (event.currentTarget.query.value.trim() === '') {//Щоб не вводити пробіли в інпуті
+        return 
+  };
+  
+  const inputValue = event.currentTarget.query.value
+  
   if (!inputValue) {
-    refs.error.classList.remove('visually-hidden')
+    refs.error.classList.remove('visually-hidden')//Можна замінити на біблотеку Notflix
   } else {
     refs.error.classList.add('visually-hidden')
   }
-  await getSearch(inputValue).then(data => {
+  await getSearch(inputValue).then(data => { //Функція запиту на бекенд по вводу в інпуті
     if (data.results.length === 0) {
-      refs.error.classList.remove('visually-hidden')
-    }
-  })
+      refs.error.classList.remove('visually-hidden')//Можна замінити на біблотеку Notflix
+    };
 
+    data.results.map((filmData) => {
+      replaceGenresById(filmData); //Функція добавляє назви жанрів по id
+    });
+
+    localStorage.setItem('search_films_from_beckend', JSON.stringify(data.results));
+    markUpPopularFilmGallery(data.results);//Рендерить розмітку запиту із форми
+    //spinner.classList.add('visually-hidden');Не працює!!!!!!!!!!!!!!!!
+
+  }).catch((error) => console.log(error.message));
+  
+  refs.searchForm.reset();
+  
+ }
+
+
+//Очистка розмітки 
+
+function clearMarkup() {
+    refs.gallery.innerHTML = '';
 }
+
+
+
+
+
+
+
+
+  
+ 
+
+
