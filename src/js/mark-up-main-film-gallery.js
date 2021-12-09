07ae7533a10a374cbdabd191b8d68cee639a15a0
Roman Broshnitskiy <romario777v8@gmail.com>
2021-12-09 23:17:11 +0200

    bag-fix-markUp-function

--- src/js/mark-up-main-film-gallery.js
+++ src/js/mark-up-main-film-gallery.js
@@ -5,16 +5,17 @@ export function markUpPopularFilmGallery(films) {
     .map((film) => {
       const { title, name, genre_ids, vote_average, release_date, first_air_date, poster_path, backdrop_path } = film;
 
+      let filmYear;
+      let filmName;
+      release_date !== undefined ? filmYear = release_date.slice(0, 4) : filmYear = first_air_date.slice(0, 4);
+      title !== undefined ? filmName = title.toUpperCase() : filmName = name.toUpperCase();
+
       return `<li class="card-item modalBtn" data-id="${film.id}" >
         <a class="link modalBtn" href="#" data-id="${film.id}" >
-        <div class=""><img class="card-image modalBtn" data-id="${
-          film.id
-        }" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="poster-film"></div>
-        <h2 class="card-title modalBtn" data-id="${film.id}">${title.toUpperCase() || name.toUpperCase()}</h2>
+        <div class=""><img class="card-image modalBtn" data-id="${film.id}" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="poster-film"></div>
+        <h2 class="card-title modalBtn" data-id="${film.id}">${filmName}</h2>
           <div class="card-description-container modalBtn" data-id="${film.id}">
-            <p class="card-description modalBtn" data-id="${film.id}">${genre_ids.join(', ')} | ${
-        release_date.slice(0, 4) || first_air_date.slice(0, 4)
-      }</p>
+            <p class="card-description modalBtn" data-id="${film.id}">${genre_ids.join(', ')} | ${filmYear}</p>
           </div>
       </a>
     </li>`;
