4f16f2936e3bc27355e08da687b4273d672df8c2
valentynadymova <valentynadymova@gmail.com>
2021-12-15 21:22:57 +0100

    btn fixed

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -1,11 +1,36 @@
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+// const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+// const rootElement = document.documentElement;
+
+// export function scrollToTop() {
+//   // Scroll to top logic
+//   rootElement.scrollTo({
+//     top: 0,
+//     behavior: 'smooth'
+//   });
+// }
+// scrollToTopBtn.addEventListener('click', scrollToTop);
+
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 const rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.1) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
 
 export function scrollToTop() {
   // Scroll to top logic
   rootElement.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth"
   });
 }
-scrollToTopBtn.addEventListener('click', scrollToTop);
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
